f9a76ae3dae5e7b1e1ed4f9e706c56c02e82197a
Richard Milne <richmilne@hotmail.com>
2017-05-02 13:54:35 +0200

    Created base class for all the Planning examples.
    All that really differs in the example air cargo and cake baking examples are
    the creation of the actions. So pass an action creation function into the class
    constructor, and all the rest of the code can be shared by the other problems!
    
    Implemented the Spare Tyre problem to give an example of how the new class and
    action creation functions could be used; will convert other problems later.

---
+++ example_spare_tire.py
@@ -0,0 +1,111 @@
+from actions import Action
+from planning_problem import PlanningProblem
+from functools import partial
+
+from aimacode.search import (
+    Node, breadth_first_search, astar_search, depth_first_graph_search,
+    uniform_cost_search, greedy_best_first_graph_search, Problem,
+)
+from lp_utils import encode_state, decode_state
+from my_planning_graph import PlanningGraph
+from run_search import run_search
+
+from functools import lru_cache, partial
+
+def spare_tire_actions(tires, locations, all_fluents):
+    def remove_actions():
+        # Action(Remove(obj, loc),
+        #    PRECOND:  At(obj, loc)
+        #    EFFECT: ¬ At(obj, loc) ∧ At(obj, Ground)
+        actions = []
+        for tyre in tires:
+            effect_add = ['At(%s, Ground)' % tyre]
+            for loc in locations:
+                precond_pos = ['At(%s, %s)' % (tyre, loc)]
+                remove = Action('Remove(%s, %s)' % (tyre, loc), all_fluents,
+                                [precond_pos, []],
+                                [effect_add, precond_pos])
+                actions.append(remove)
+        return actions
+        
+    def put_on_actions():
+        # Action(PutOn(t, Axle),
+        #    PRECOND:  Tire(t) ∧ At(t, Ground) ∧  ¬ At(Flat, Axle)
+        #    EFFECT: ¬ At(t, Ground) ∧ At(t, Axle)
+
+        # What stops you from putting the Flat back on an Axle, even if the
+        # Spare is already there? The fact that a solution will be found
+        # before that possibility arises?
+        actions = []
+        precond_neg = ['At(Flat, Axle)']
+        for tyre in tires:
+            precond_pos = ['At(%s, Ground)' % tyre]
+            effect_add = ['At(%s, Axle)' % tyre]
+            put_on = Action('PutOn(%s, Axle)' % tyre, all_fluents,
+                            [precond_pos, precond_neg],
+                            [effect_add, precond_pos])
+            actions.append(put_on)
+        return actions
+
+    def leave_overnight_action():
+        # Action(LeaveOvernight,
+        #    PRECOND:
+        #    EFFECT: ¬ At(Spare, Ground) ∧ ¬ At(Spare, Axle)
+        #            ¬ At(Spare, Trunk)  ∧ ¬ At(Flat, Ground)
+        #            ¬ At(Flat, Axle)    ∧ ¬ At(Flat, Trunk)
+        removals = []
+        for tyre in tires:
+            for loc in locations:
+                removals.append('At(%s, %s)' % (tyre, loc))
+
+        leave_overnight = Action('LeaveOvernight', all_fluents,
+                                 [[], []],
+                                 [[], removals])
+        return [leave_overnight]
+
+    return remove_actions() + put_on_actions() + leave_overnight_action()
+
+def spare_tire():
+    tires = ['Spare', 'Flat']
+    locations = ['Ground', 'Axle', 'Trunk']
+
+    pos = ['At(Flat, Axle)', 'At(Spare, Trunk)']
+    neg = ['At(Flat, Ground)', 'At(Flat, Trunk)',
+           'At(Spare, Ground)', 'At(Spare, Axle)']
+    goal = ['At(Spare, Axle)']
+    init = (pos, neg)
+    action_fn = partial(spare_tire_actions, tires, locations)
+    return PlanningProblem(init, goal, action_fn)
+
+
+if __name__ == '__main__':
+    p = spare_tire()
+    print("**** Spare Tire example problem setup ****")
+    initial = decode_state(p.all_fluents, p.initial)
+    print("Initial state for this problem is {}".format(initial))
+    print("Actions for this domain are:")
+    for a in p.actions_list:
+        print('  ', str(a))
+
+    print("Fluents in this problem are:")
+    for f in p.all_fluents:
+        print('   {}'.format(f))
+
+    print("Goal requirement for this problem are:")
+    for g in decode_state(p.all_fluents, p.goal):
+        print('   {}'.format(g))
+    print()
+    print("*** Breadth First Search")
+    run_search(p, breadth_first_search)
+    print("*** Depth First Search")
+    run_search(p, depth_first_graph_search)
+    print("*** Uniform Cost Search")
+    run_search(p, uniform_cost_search)
+    print("*** Greedy Best First Graph Search - null heuristic")
+    run_search(p, greedy_best_first_graph_search, parameter=p.h_1)
+    print("*** A-star null heuristic")
+    run_search(p, astar_search, p.h_1)
+    # print("A-star ignore preconditions heuristic")
+    # rs(p, "astar_search - ignore preconditions heuristic", astar_search, p.h_ignore_preconditions)
+    # print(""A-star levelsum heuristic)
+    # rs(p, "astar_search - levelsum heuristic", astar_search, p.h_pg_levelsum)
--- planning_problem.py
+++ planning_problem.py
@@ -1,136 +1,32 @@
 from actions import Action
-from lp_utils import encode_state, decode_state
+from lp_utils import encode_state
 from aimacode.search import Node, Problem
-
 from my_planning_graph import PlanningGraph
 
 from functools import lru_cache
 
 
-class AirCargoProblem(Problem):
-    def __init__(self, cargos, planes, airports, initial, goal):
+class PlanningProblem(Problem):
+    def __init__(self, initial, goal, action_fn):
         """
-
-        :param cargos: list of str
-            cargos in the problem
-        :param planes: list of str
-            planes in the problem
-        :param airports: list of str
-            airports in the problem
-        :param initial: FluentState object
-            positive and negative literal fluents (as expr) describing
-            initial state
-        :param goal: list of expr
-            literal fluents required for goal test"""
+        :param initial: 2-tuple of positive and negative literal fluents,
+            describing the initial state of the problem
+        :param goal: list
+            literal fluents required for goal test
+        :param action_fn: Function which creates all the concrete actions for
+            the problem. This function will be passed an ordered, immutable
+            reference tuple of all the fluents in the problem. The function
+            will only be called once, during class construction."""
         pos, neg = initial
         all_fluents = tuple(sorted(pos+neg))
         initial_state = encode_state(all_fluents, pos)
         goal = encode_state(all_fluents, goal)
 
         self.all_fluents = all_fluents
-        self.initial_state = initial_state
+        # self.initial_state = initial_state - not needed; initial state
+        # saved in Parent class as .initial
         Problem.__init__(self, initial_state, goal=goal)
-        self.cargos = cargos
-        self.planes = planes
-        self.airports = airports
-        self.actions_list = self.get_actions()
-
-    def get_actions(self):
-        """
-        This method creates concrete actions (no variables) for all actions
-        in the problem domain action schema and turns them into complete
-        Action objects as defined in the aimacode.planning module. It is
-        computationally expensive to call this method directly; however, it
-        is called in the constructor and the results cached in the
-        `actions_list` property.
-
-        Returns:
-        ----------
-        list<Action>
-            list of Action objects"""
-        # TODO create concrete Action objects based on the domain action schema
-        # for: Load, Unload, and Fly
-        # concrete actions definition: specific literal action that does not
-        # include variables as with the schema. For example, the action schema
-        # 'Load(c, p, a)' can represent the concrete actions 'Load(C1, P1, SFO)'
-        # or 'Load(C2, P2, JFK)'.  The actions for the planning problem must be
-        # concrete because the problems in forward search and Planning Graphs
-        # must use Propositional Logic
-
-        def load_actions():
-            """Create all concrete Load actions and return a list
-
-            :return: list of Action objects"""
-            # Action(Load(c, p, a),
-            #    PRECOND: At(c, a) ∧ At(p, a) ∧ Cargo(c) ∧ Plane(p) ∧ Airport(a)
-            #    EFFECT: ¬ At(c, a) ∧ In(c, p))
-            loads = []
-            all_fluents = self.all_fluents
-
-            for cargo in self.cargos:
-                for plane in self.planes:
-                    for airport in self.airports:
-                        precond = ['At(%s, %s)' % (cargo, airport)]
-                        precond.append('At(%s, %s)' % (plane, airport))
-                        effect_add = ['In(%s, %s)' % (cargo, plane)]
-                        effect_rem = ['At(%s, %s)' % (cargo, airport)]
-                        load = 'Load(%s, %s, %s)' % (cargo, plane, airport)
-                        action = Action(load, all_fluents,
-                                        [precond, []],
-                                        [effect_add, effect_rem])
-                        loads.append(action)
-            return loads
-
-        def unload_actions():
-            """Create all concrete Unload actions and return a list
-
-            :return: list of Action objects"""
-            # Action(Unload(c, p, a),
-            #    PRECOND: In(c, p) ∧ At(p, a) ∧ Cargo(c) ∧ Plane(p) ∧ Airport(a)
-            #    EFFECT: At(c, a) ∧ ¬ In(c, p))
-            unloads = []
-            all_fluents = self.all_fluents
-
-            for cargo in self.cargos:
-                for plane in self.planes:
-                    for airport in self.airports:
-                        precond = ['In(%s, %s)' % (cargo, plane)]
-                        precond.append('At(%s, %s)' % (plane, airport))
-                        effect_rem = ['In(%s, %s)' % (cargo, plane)]
-                        effect_add = ['At(%s, %s)' % (cargo, airport)]
-                        unload = 'Unload(%s, %s, %s)' % (cargo, plane, airport)
-                        action = Action(unload, all_fluents,
-                                        [precond, []],
-                                        [effect_add, effect_rem])
-                        unloads.append(action)
-            return unloads
-
-        def fly_actions():
-            """Create all concrete Fly actions and return a list
-
-            :return: list of Action objects"""
-            # Action(Fly(p, from, to),
-            #    PRECOND: At(p, from) ∧ Plane(p) ∧ Airport(from) ∧ Airport(to)
-            #    EFFECT: ¬ At(p, from) ∧ At(p, to))
-            flys = []
-            all_fluents = self.all_fluents
-
-            for fr in self.airports:
-                for to in self.airports:
-                    if fr != to:
-                        for p in self.planes:
-                            precond_pos = ["At({}, {})".format(p, fr)]
-                            precond_neg = []
-                            effect_add = ["At({}, {})".format(p, to)]
-                            effect_rem = ["At({}, {})".format(p, fr)]
-                            fly = Action("Fly({}, {}, {})".format(p, fr, to),
-                                         all_fluents,
-                                         [precond_pos, precond_neg],
-                                         [effect_add, effect_rem])
-                            flys.append(fly)
-            return flys
-
-        return load_actions() + unload_actions() + fly_actions()
+        self.actions_list = action_fn(all_fluents)
 
     def actions(self, state: int) -> list:
         """Return the actions that can be executed in the given state.
@@ -187,90 +83,3 @@ class AirCargoProblem(Problem):
         # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
         count = 0
         return count
-
-
-def air_cargo_p1() -> AirCargoProblem:
-    cargos = ['C1', 'C2']
-    planes = ['P1', 'P2']
-    airports = ['JFK', 'SFO']
-    pos = ['At(C1, SFO)',
-           'At(C2, JFK)',
-           'At(P1, SFO)',
-           'At(P2, JFK)',
-           ]
-    neg = ['At(C2, SFO)',
-           'In(C2, P1)',
-           'In(C2, P2)',
-           'At(C1, JFK)',
-           'In(C1, P1)',
-           'In(C1, P2)',
-           'At(P1, JFK)',
-           'At(P2, SFO)',
-           ]
-    init = (pos, neg)
-    goal = ['At(C1, JFK)',
-            'At(C2, SFO)',
-            ]
-    return AirCargoProblem(cargos, planes, airports, init, goal)
-
-
-def cross_product(function, objects1, objects2):
-    products = []
-    for obj1 in objects1:
-        for obj2 in objects2:
-            products.append('%s(%s, %s)' % (function, obj1, obj2))
-    return set(products)
-
-
-def create_problem(cargos, planes, airports, pos_cargos, pos_planes, goals):
-    all_cargos = cross_product('At', cargos, airports)
-    neg_cargos = all_cargos - set(pos_cargos)
-
-    all_planes = cross_product('At', planes, airports)
-    neg_planes = all_planes - set(pos_planes)
-
-    neg_loads = cross_product('In', cargos, planes)
-
-    expr_list = [
-        pos_cargos + pos_planes,
-        sorted(sum([list(n) for n in [neg_cargos, neg_planes, neg_loads]], [])),
-        goals
-    ]
-    pos, neg, goals = expr_list
-
-    if 0:
-        for label, obj in [('pos', pos), ('neg', neg), ('goals', goals)]:
-            print()
-            print(label+':')
-            print(type(obj[0]), obj)
-
-    init = (pos, neg)
-    return AirCargoProblem(cargos, planes, airports, init, goals)
-
-
-def air_cargo_p2() -> AirCargoProblem:
-    cargos = ['C1', 'C2', 'C3']
-    planes = ['P1', 'P2', 'P3']
-    airports = ['JFK', 'SFO', 'ATL']
-
-    pos_cargos = ['At(C1, SFO)', 'At(C2, JFK)', 'At(C3, ATL)']
-    pos_planes = ['At(P1, SFO)', 'At(P2, JFK)', 'At(P3, ATL)']
-
-    goals = ['At(C1, JFK)', 'At(C2, SFO)', 'At(C3, SFO)']
-
-    args = (cargos, planes, airports, pos_cargos, pos_planes, goals)
-    return create_problem(*args)
-
-
-def air_cargo_p3() -> AirCargoProblem:
-    cargos = ['C1', 'C2', 'C3', 'C4']
-    planes = ['P1', 'P2']
-    airports = ['JFK', 'SFO', 'ATL', 'ORD']
-
-    pos_cargos = ['At(C1, SFO)', 'At(C2, JFK)', 'At(C3, ATL)', 'At(C4, ORD)']
-    pos_planes = ['At(P1, SFO)', 'At(P2, JFK)']
-
-    goals = ['At(C1, JFK)', 'At(C3, JFK)', 'At(C2, SFO)', 'At(C4, SFO)']
-
-    args = (cargos, planes, airports, pos_cargos, pos_planes, goals)
-    return create_problem(*args)
