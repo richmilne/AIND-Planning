4a3458cf3fc0524d32c3781478eb0a9d017c1dea
Richard Milne <richmilne@hotmail.com>
2017-05-03 12:40:24 +0200

    Added mutex tests to Action class.

--- actions.py
+++ actions.py
@@ -1,5 +1,6 @@
 from lp_utils import encode_state, action_bitmaps, decode_state
 
+
 class Action(object):
     """Defines an action schema using preconditions and effects
 
@@ -45,10 +46,35 @@ class Action(object):
     """
     def __init__(self, name, all_fluents, precond, effect):
         self.name = name
-        precond = action_bitmaps(all_fluents, *precond)
-        self.pos_bitmap, self.neg_bitmap = precond[:2]
-        effect = action_bitmaps(all_fluents, *effect)
-        self.effect_add, self.effect_rem = effect[0], effect[2]
+        self.all_fluents = all_fluents
+
+        pos, neg, _ = action_bitmaps(all_fluents, *precond)
+        self.precond_pos, self.precond_neg = pos, neg
+
+        add, rem, mask = action_bitmaps(all_fluents, *effect)
+        self.effect_add, self.effect_rem, self.effect_mask = add, rem, mask
+
+        assert pos & neg == 0
+
+        # You can only remove what was previously asserted or given in the
+        # preconditions... or can you? Can you have an effect which removes
+        # something that isn't there, as an assurance that it will never be
+        # there?
+        # For answer, see the Spare Tire Problem (Figure 10.2 3rd ed AIMA).
+        # If you leave car out overnight, everything is retracted, no matter
+        # what state it was before.
+        # assert (pos & rem == rem)
+
+        # Can have an example where a removal is the negative of an add.
+        # The effects of Remove(Flat, Ground) are
+        # ¬ At(Flat, Ground) and At(Flat, Ground)
+        # It works because in the action below, we do the removal first, then
+        # we add in the new literal(s).
+
+        # If the problem has cases like this, either comment out assertion
+        # below, or modify the Action creation function so as not to produce
+        # redundant, no-op actions like this.
+        assert add & rem == 0
 
     def __call__(self, kb):
         return self.act(kb)
@@ -59,9 +85,9 @@ class Action(object):
     def check_precond(self, kb):
         """Checks if the precondition is satisfied in the current state"""
         # KB - recorded as state bitmap
-        if not (kb & self.pos_bitmap == self.pos_bitmap):
+        if not (kb & self.precond_pos == self.precond_pos):
             return False
-        return kb & self.neg_bitmap == 0
+        return kb & self.precond_neg == 0
 
     def act(self, kb):
         """Executes the action on the state's kb"""
@@ -69,11 +95,39 @@ class Action(object):
         if not self.check_precond(kb):
             raise Exception("Action pre-conditions not satisfied")
         # remove negative literals
-        kb = kb & self.effect_rem
+        kb = kb & self.effect_mask
         # add positive literals
         kb = kb | self.effect_add
 
         return kb
+
+    def expand_bitmap(self, bitmap):
+        """Returns the fluent literals corresponding to the bits set in bitmap.
+        """
+        return decode_state(self.all_fluents, bitmap)
+
+    def mutex_inconsistent_effects(self, other):
+        """Test a pair of actions for inconsistent effects.
+
+        Returns True if one action negates an effect of the other; False
+        otherwise."""
+        return     ( (self.effect_add & other.effect_rem) |
+                     (self.effect_rem & other.effect_add) )
+
+    def mutex_interference(self, other):
+        # one of the effects of one action is the negation of a precondition
+        # of the other.
+        return     ( (self.precond_pos & other.effect_rem) |
+                     (self.precond_neg & other.effect_add) )
+
+    def mutex_competing(self, other):
+        """Test a pair of actions for competing mutual exclusion
+
+        Returns True if any of the preconditions of one action are mutually
+        exclusive with any of the preconditions of the other action."""
+        return     ( (self.precond_pos & other.precond_neg) |
+                     (self.precond_neg & other.precond_pos) )
+
 
 if __name__ == '__main__':
     import doctest
