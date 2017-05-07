5e5af8eeed4505ce57f262ee424592dc13ef0a73
Richard Milne <richmilne@hotmail.com>
2017-05-08 00:15:37 +0200

    Tidied up imports

--- tests/test_my_planning_graph.py
+++ tests/test_my_planning_graph.py
@@ -1,15 +1,10 @@
 import os
 import sys
 
-parent = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(os.path.dirname(parent), "aimacode"))
 import unittest
-from aimacode.utils import expr
 from actions import Action
 from example_have_cake import have_cake
-from my_planning_graph import (
-    PlanningGraph, PgNode_a, PgNode_s, mutexify
-)
+from my_planning_graph import PlanningGraph, PgNode_a, PgNode_s, mutexify
 from planning_problem import PlanningProblem
 
 class TestPlanningGraphLevels(unittest.TestCase):
